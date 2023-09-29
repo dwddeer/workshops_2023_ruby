4185e3dcbb82cb9a07991796315ad20b0ac0d52a
dwddeer <jeleniogrod@gmail.com>
2023-09-29 19:36:34 +0200

    all done

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,15 @@
 class CountElements
-
+    def initialize(array)
+        @array = array
+    end
+    
+    def perform
+        result = Hash.new(0) 
+    
+        @array.each do |element|
+            result[element] += 1 
+        end
+    
+        result 
+    end
 end
